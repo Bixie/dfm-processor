00662b04db87dd33e9169035c287ced8abf0c5f7
Matthijs Alles <admin@bixie.nl>
2018-08-17 22:25:09 +0200

    fix stringvalue env

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ exports.DFM_APP_PATH = process.env.DFM_APP_PATH || 'C:/Projects/DFM/dfm-processo
 exports.PARAMSFILES_PATH = process.env.DFM_PARAMSFILES_PATH || 'C:/Projects/DFM/params-in';
 exports.IMAGEFILES_OUTPUT_PATH = process.env.DFM_IMAGEFILES_OUTPUT_PATH || 'C:/Projects/DFM/image-out';
 exports.IMAGEFILES_SENT_PATH = process.env.DFM_IMAGEFILES_SENT_PATH || 'C:/Projects/DFM/image-sent';
-exports.WEBSERVER_LOCAL_PATH = process.env.DFM_WEBSERVER_LOCAL_PATH || false;
+exports.WEBSERVER_LOCAL_PATH = process.env.DFM_WEBSERVER_LOCAL_PATH !== 'false' ? process.env.DFM_WEBSERVER_LOCAL_PATH : false;
 
 exports.INTERNAL_API_HOST = process.env.DFM_INTERNAL_API_HOST || 'https://www.enterergodics.com/dfm-api/index.php';
 exports.INTERNAL_API_KEY = process.env.DFM_INTERNAL_API_KEY || 'gh*sgSHDsdg#$^34W^sVS(#$SG*$4g9gGR&3';
