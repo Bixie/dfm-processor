8e65ebeea38bc838a4adf0fe7fa9b73a87fbeb06
Matthijs Alles <matthijs@bixie.nl>
2020-04-11 14:06:47 +0200

    add license key to filename, update params

--- src/params-file.js
+++ src/params-file.js
@@ -4,19 +4,41 @@ const Promise = require('bluebird');
 const LF = '\r\n';
 
 const defaultParams = {
-    Investment: 5,
-    PortfolioSize: 3,
-    HoldingPeriod: 3,
-    ValidationPeriod: 8,
-    PennyStocks: 2,
-    GrowthPotential: 1,
-    HedgePercentage: 1,
-    BalanceRR: 1,
-    Watchlists: 4,
-    TransactionCosts: 1,
-    LoanPercentage: 1,
-    DividendTax: 4,
-    DataProvider: 1,
+    DataProvider: 'CSI',
+    IncludeInactive: 1,
+    Benchmark: 'SP500',
+    Watchlists: 'Safe',
+    TradingLiquidity: '1',
+    HistoricalPrice: '5',
+    AdjustedPrice: '0.5',
+    MARRatio: 'N/A',
+    Trend: 'N/A',
+    TrendPeriod: 'N/A',
+    Ranking: 'reversing',
+    ShortCorrelation: 'N/A',
+    LongCorrelation: 'N/A',
+    Investment: 48,
+    PortfolioSize: 12,
+    LongShort: 'Long',
+    HoldingPeriod: '13',
+    ValidationPeriod: 30,
+    InvestementObjective: 'MaxMAR',
+    PriceWeighing: 'Adjusted',
+    HedgePercentage: 35,
+    LowerBound: '0.1',
+    SetupPeriod: 9,
+    WeightInterval: 1,
+    OptimalizationTechnique: 'LongThenShort',
+    TransactionCosts: '4',
+    LoanPercentage: '0.5',
+    DividendTax: '15'
+};
+
+const defaultOptions = {
+    width: 1200,
+    layout: 'default',
+    locale: 'nl-NL',
+    licenseKey: '',
 };
 
 function formatLine(value, key) {
@@ -30,17 +52,14 @@ class ParamsFile {
 
     constructor(id, params, options = {}) {
         this.id = id;
-        this.params = {};
-        //we can just test for falsy value, since the indices are 1 based
-        Object.entries(defaultParams)
-            .forEach(([key, defaultValue,]) => this.params[key] = params[key] || defaultValue);
-        this.options = options;
+        this.params = {...defaultParams, ...params,};
+        this.options = {...defaultOptions, ...options,};
     }
 
     render() {
         let output = '';
         output += Object.keys(this.params).map(key => {
-            return formatLine(this.params[key]);
+            return formatLine(this.params[key], key);
         }).join(LF);
         // output += LF;
         // output += Object.keys(this.options).map(key => {
@@ -52,7 +71,7 @@ class ParamsFile {
     write(folderpath) {
         const language = this.options.locale === 'nl-NL' ? 'NL' : 'EN';
         return new Promise((resolve, reject) => {
-            const filename = `${folderpath}/${this.id}_${language}.txt`;
+            const filename = `${folderpath}/${this.id}_${language}_${this.options.licenseKey}.txt`;
             fs.writeFile(filename, this.render(), err => {
                 if (err) {
                     reject(err);
