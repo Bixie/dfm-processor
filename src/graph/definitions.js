e3e9ba1cf070abfaffcedc17610c1741492f7c2c
Matthijs Alles <matthijs@bixie.nl>
2020-08-29 18:36:29 +0200

    add graphs

--- src/graph/definitions.js
+++ src/graph/definitions.js
@@ -7,27 +7,27 @@ module.exports = {
             {
                 filename: 'chart_compound_equ_w_f.txt',
                 className: 'equ_w_f',
-                color: '#95b2c0',
+                type: 'line',
             },
             {
                 filename: 'chart_compound_equ_w_m.txt',
                 className: 'equ_w_m',
-                color: '#d98f8b',
+                type: 'line',
             },
             {
                 filename: 'chart_compound_pr_w_asp_m.txt',
                 className: 'pr_w_asp_m',
-                color: '#b4aea3',
+                type: 'line',
             },
             {
                 filename: 'chart_compound_random_500_f.txt',
                 className: 'random_500_f',
-                color: '#c2c383',
+                type: 'line',
             },
             {
                 filename: 'chart_compound_s_p_500.txt',
                 className: 's_p_500',
-                color: '#e0b093',
+                type: 'line',
             },
         ],
     },
@@ -39,59 +39,234 @@ module.exports = {
             {
                 filename: 'chart_constant_equ_w_m_cashflow.txt',
                 className: 'equ_w_m_cashflow',
-                color: '#95b2c0',
+                type: 'line',
             },
             {
                 filename: 'chart_constant_equ_w_m_net_liquidation_value.txt',
                 className: 'equ_w_m_net_liquidation_value',
-                color: '#d98f8b',
+                type: 'line',
             },
             {
                 filename: 'chart_constant_opt_w_cashflow.txt',
                 className: 'opt_w_cashflow',
-                color: '#b4aea3',
+                type: 'line',
             },
             {
                 filename: 'chart_constant_opt_w_net_liquidation_value.txt',
                 className: 'opt_w_net_liquidation_value',
-                color: '#c2c383',
+                type: 'line',
             },
             {
                 filename: 'chart_constant_pr_w_asp_m_cashflow.txt',
                 className: 'pr_w_asp_m_cashflow',
-                color: '#e0b093',
+                type: 'line',
             },
             {
                 filename: 'chart_constant_pr_w_asp_m_net_liquidation_value.txt',
                 className: 'prw_m_net_liquidation',
-                color: '#8a8a8a',
+                type: 'line',
             },
             {
                 filename: 'chart_constant_s_p_500_cashflow.txt',
                 className: 'sandp500_cashflow',
-                color: '#fbd6a9',
+                type: 'line',
             },
         ],
     },
-    chart_miscellaneous: {
+    chart_volatility: {
         viewBox: {width: 455, height: 225,},
         margin: {top: 10, right: 10, bottom: 50, left: 50,},
         axes: {x: {type: 'time',}, y: {type: 'linear',},},
         dataSets: [
             {
-                filename: 'chart_miscellaneous_stocks_before_ranking.txt',
-                className: 'stocks_before_ranking',
-                color: '#95b2c0',
-            },
-            {
-                filename: 'chart_miscellaneous_stocks_required.txt',
-                className: 'stocks_required',
-                color: '#d98f8b',
-            },
-            {
-                filename: 'chart_miscellaneous_volatility.txt',
+                filename: 'chart_volatility_volatility.txt',
                 className: 'volatility',
-                color: '#c2c383',
+                type: 'line',
+            },
+        ],
+    },
+    chart_timing_equ_w_f_comp: {
+        viewBox: {width: 455, height: 225,},
+        margin: {top: 10, right: 10, bottom: 50, left: 50,},
+        axes: {x: {type: 'time',}, y: {type: 'log',},},
+        dataSets: [
+            {
+                filename: 'chart_timing_equ_w_f_comp_timing_equ_w_f.txt',
+                className: 'default',
+                type: 'line',
+            },
+            {
+                filename: 'chart_timing_equ_w_f_comp_best_timing_equ_w_f.txt',
+                className: 'best',
+                type: 'line',
+            },
+            {
+                filename: 'chart_timing_equ_w_f_comp_worst_timing_equ_w_f.txt',
+                className: 'worst',
+                type: 'line',
+            },
+        ],
+    },
+    chart_timing_equ_w_m_comp: {
+        viewBox: {width: 455, height: 225,},
+        margin: {top: 10, right: 10, bottom: 50, left: 50,},
+        axes: {x: {type: 'time',}, y: {type: 'log',},},
+        dataSets: [
+            {
+                filename: 'chart_timing_equ_w_m_comp_timing_equ_w_m.txt',
+                className: 'default',
+                type: 'line',
+            },
+            {
+                filename: 'chart_timing_equ_w_m_comp_best_timing_equ_w_m.txt',
+                className: 'best',
+                type: 'line',
+            },
+            {
+                filename: 'chart_timing_equ_w_m_comp_worst_timing_equ_w_m.txt',
+                className: 'worst',
+                type: 'line',
+            },
+        ],
+    },
+    chart_timing_pr_w_asp_m_comp: {
+        viewBox: {width: 455, height: 225,},
+        margin: {top: 10, right: 10, bottom: 50, left: 50,},
+        axes: {x: {type: 'time',}, y: {type: 'log',},},
+        dataSets: [
+            {
+                filename: 'chart_timing_pr_w_asp_m_comp_timing_pr_w_asp_m.txt',
+                className: 'default',
+                type: 'line',
+            },
+            {
+                filename: 'chart_timing_pr_w_asp_m_comp_best_timing_pr_w_asp_m.txt',
+                className: 'best',
+                type: 'line',
+            },
+            {
+                filename: 'chart_timing_pr_w_asp_m_comp_worst_timing_pr_w_asp_m.txt',
+                className: 'worst',
+                type: 'line',
+            },
+        ],
+    },
+    chart_timing_equ_w_m_constant: {
+        viewBox: {width: 455, height: 225,},
+        margin: {top: 10, right: 10, bottom: 50, left: 50,},
+        axes: {x: {type: 'time',}, y: {type: 'linear',},},
+        dataSets: [
+            {
+                filename: 'chart_timing_equ_w_m_constant_timing_equ_w_m_cashflow.txt',
+                className: 'default',
+                type: 'line',
+            },
+            {
+                filename: 'chart_timing_equ_w_m_constant_best_timing_equ_w_m_cashflow.txt',
+                className: 'best',
+                type: 'line',
+            },
+            {
+                filename: 'chart_timing_equ_w_m_constant_worst_timing_equ_w_m_cashflow.txt',
+                className: 'worst',
+                type: 'line',
+            },
+        ],
+    },
+    chart_timing_pr_w_asp_m_constant: {
+        viewBox: {width: 455, height: 225,},
+        margin: {top: 10, right: 10, bottom: 50, left: 50,},
+        axes: {x: {type: 'time',}, y: {type: 'linear',},},
+        dataSets: [
+            {
+                filename: 'chart_timing_pr_w_asp_m_constant_timing_pr_w_asp_m_cashflow.txt',
+                className: 'default',
+                type: 'line',
+            },
+            {
+                filename: 'chart_timing_pr_w_asp_m_constant_best_timing_pr_w_asp_m_cashflow.txt',
+                className: 'best',
+                type: 'line',
+            },
+            {
+                filename: 'chart_timing_pr_w_asp_m_constant_worst_timing_pr_w_asp_m_cashflow.txt',
+                className: 'worst',
+                type: 'line',
+            },
+        ],
+    },
+    chart_timing_opt_w_constant: {
+        viewBox: {width: 455, height: 225,},
+        margin: {top: 10, right: 10, bottom: 50, left: 50,},
+        axes: {x: {type: 'time',}, y: {type: 'linear',},},
+        dataSets: [
+            {
+                filename: 'chart_timing_opt_w_constant_timing_opt_w_cashflow.txt',
+                className: 'default',
+                type: 'line',
+            },
+            {
+                filename: 'chart_timing_opt_w_constant_best_timing_opt_w_cashflow.txt',
+                className: 'best',
+                type: 'line',
+            },
+            {
+                filename: 'chart_timing_opt_w_constant_worst_timing_opt_w_cashflow.txt',
+                className: 'worst',
+                type: 'line',
+            },
+        ],
+    },
+    chart_nr_stocks_a_screening: {
+        viewBox: {width: 455, height: 225,},
+        margin: {top: 10, right: 10, bottom: 50, left: 50,},
+        axes: {x: {type: 'time',}, y: {type: 'linear',},},
+        dataSets: [
+            {
+                filename: 'chart_nr_stocks_a_screening_stocks_before_ranking.txt',
+                className: 'stocks_before_ranking',
+                type: 'line',
+            },
+            {
+                filename: 'chart_nr_stocks_a_screening_stocks_required.txt',
+                className: 'stocks_required',
+                type: 'line',
+            },
+        ],
+    },
+    chart_return_distribution_equ_w: {
+        viewBox: {width: 455, height: 225,},
+        margin: {top: 10, right: 10, bottom: 50, left: 50,},
+        axes: {x: {type: 'linear',}, y: {type: 'linear',},},
+        dataSets: [
+            {
+                filename: 'chart_return_distribution_equ_w_return_distribution_equ_w.txt',
+                className: 'return_distribution',
+                type: 'bar',
+            },
+        ],
+    },
+    chart_return_distribution_pr_w: {
+        viewBox: {width: 455, height: 225,},
+        margin: {top: 10, right: 10, bottom: 50, left: 50,},
+        axes: {x: {type: 'linear',}, y: {type: 'linear',},},
+        dataSets: [
+            {
+                filename: 'chart_return_distribution_pr_w_return_distribution_pr_w.txt',
+                className: 'return_distribution',
+                type: 'bar',
+            },
+        ],
+    },
+    chart_return_distribution_opt_w: {
+        viewBox: {width: 455, height: 225,},
+        margin: {top: 10, right: 10, bottom: 50, left: 50,},
+        axes: {x: {type: 'linear',}, y: {type: 'linear',},},
+        dataSets: [
+            {
+                filename: 'chart_return_distribution_opt_w_return_distribution_opt_w.txt',
+                className: 'return_distribution',
+                type: 'bar',
             },
         ],
     },
